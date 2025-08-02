79dbe766233b8eb702a27826db09a340fd080d9c
SufyaanR <221075127@mycput.ac.za>
2025-08-02 12:06:45 +0200

    Test Git user commit

--- src/main/java/za/ac/cput/domain/Car.java
+++ src/main/java/za/ac/cput/domain/Car.java
@@ -1,4 +1,12 @@
 package za.ac.cput.domain;
 
+import jakarta.persistence.Entity;
+/* Car.java
+     Car POJO class
+     Author: S Rawoot (221075127)
+     Date: 02 August 2025 */
+@Entity
 public class Car {
+
+
 }
