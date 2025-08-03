b3d237b866b09c41720d3559633c9e7d69210045
222614153 <222614153@mycput.ac.za>
2025-08-03 14:28:30 +0200

    Implement ProUser and PaymentStatus classes with JPA support
    - Add ProUser entity extending User base class
    - Implement PaymentStatus enum (PENDING/SUCCESSFUL/FAILED)
    - Include builder pattern and JPA annotations

--- src/main/java/za/ac/cput/domain/PaymentStatus.java
+++ src/main/java/za/ac/cput/domain/PaymentStatus.java
@@ -1,4 +1,16 @@
 package za.ac.cput.domain;
 
+/**
+ * Enum representing payment states
+ * Author: Nompumezo Mcatshulwa (222614153)
+ */
 public enum PaymentStatus {
+    PENDING,
+    SUCCESSFUL,
+    FAILED;
+
+    @Override
+    public String toString() {
+        return name();
+    }
 }
--- src/main/java/za/ac/cput/domain/ProUser.java
+++ src/main/java/za/ac/cput/domain/ProUser.java
@@ -1,4 +1,74 @@
 package za.ac.cput.domain;
 
-public class ProUser {
+import jakarta.persistence.Entity;
+import jakarta.persistence.DiscriminatorValue;
+
+/**
+ * Professional user entity extending base User
+ * Author: Nompumezo Mcatshulwa (222614153)
+ */
+@Entity
+@DiscriminatorValue("PRO_USER")
+public class ProUser extends User {
+    private String businessName;
+    private String businessRegistrationNumber;
+
+    protected ProUser() {} // JPA required
+
+    private ProUser(Builder builder) {
+        super(builder);
+        this.businessName = builder.businessName;
+        this.businessRegistrationNumber = builder.businessRegistrationNumber;
+    }
+
+    // Getters
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public String getBusinessRegistrationNumber() {
+        return businessRegistrationNumber;
+    }
+
+    /**
+     * Builder pattern implementation
+     */
+    public static class Builder extends User.Builder {
+        private String businessName;
+        private String businessRegistrationNumber;
+
+        public Builder setBusinessName(String businessName) {
+            this.businessName = businessName;
+            return this;
+        }
+
+        public Builder setBusinessRegistrationNumber(String regNumber) {
+            this.businessRegistrationNumber = regNumber;
+            return this;
+        }
+
+        @Override
+        public ProUser build() {
+            return new ProUser(this);
+        }
+
+        @Override
+        public Builder copy(User user) {
+            super.copy(user);
+            if (user instanceof ProUser) {
+                ProUser proUser = (ProUser) user;
+                this.businessName = proUser.businessName;
+                this.businessRegistrationNumber = proUser.businessRegistrationNumber;
+            }
+            return this;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "ProUser{userId=%d, businessName='%s', registration='%s'}",
+                getUserId(), businessName, businessRegistrationNumber
+        );
+    }
 }
