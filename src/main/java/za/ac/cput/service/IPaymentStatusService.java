dc83020ac207d7ecb02ae46a4a4e292d46247dc8
222614153 <222614153@mycput.ac.za>
2025-08-13 17:11:48 +0200

    Deleted all the payment status files

--- src/main/java/za/ac/cput/service/IPaymentStatusService.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.service;
-
-import za.ac.cput.domain.PaymentStatus;
-import java.util.List;
-
-public interface IPaymentStatusService {
-    List<PaymentStatus> getAll();
-    PaymentStatus findByName(String name);
-}
