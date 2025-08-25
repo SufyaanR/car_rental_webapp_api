ab463151f9283d02f6f32b2c36bebf4cbf664442
bruneez <apollisbruneez@gmail.com>
2025-08-25 12:28:31 +0200

    domain and factory plus repository

--- src/main/java/za/ac/cput/factory/BasicUserFactory.java
+++ src/main/java/za/ac/cput/factory/BasicUserFactory.java
@@ -1,47 +1,25 @@
 package za.ac.cput.factory;
 
-import za.ac.cput.domain.BasicUser;
-import za.ac.cput.util.Helper;
-
 import java.time.LocalDate;
+import za.ac.cput.domain.BasicUser;
+import za.ac.cput.domain.UserType;
 
 public class BasicUserFactory {
-    public static BasicUser createBasicUser(
 
-            String firstName,
-            String lastName,
-            String email,
-            LocalDate dateOfBirth,
-            String idNumber,
-            String phoneNumber,
-            String userName,
-            String password
-    ) {
-        if (Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName) || Helper.isNullOrEmpty(email)
-                || Helper.isNullOrEmpty(idNumber) || Helper.isNullOrEmpty(phoneNumber)
-                || Helper.isNullOrEmpty(userName) || Helper.isNullOrEmpty(password) || dateOfBirth == null) {
-            throw new IllegalArgumentException("One or more required fields are null or empty.");
-        }
-
-        if (!Helper.isValidEmail(email)) {
-            throw new IllegalArgumentException("Invalid email format.");
-        }
-
-        if (!Helper.isValidPhoneNumber(phoneNumber)) {
-            throw new IllegalArgumentException("Invalid phone number.");
-        }
-
+    public static BasicUser create(String firstName, String lastName, String username,
+                                   String password, String email, String phoneNumber,
+                                   LocalDate dateOfBirth, String idNumber, Boolean login) {
         return new BasicUser.Builder()
                 .setFirstName(firstName)
                 .setLastName(lastName)
+                .setUsername(username)
+                .setPassword(password)
                 .setEmail(email)
+                .setPhoneNumber(phoneNumber)
                 .setDateOfBirth(dateOfBirth)
                 .setIdNumber(idNumber)
-                .setPhoneNumber(phoneNumber)
-                .setUserName(userName)
-                .setPassword(password)
+                .setLogin(login)
+                .setUserType(UserType.BASIC)
                 .build();
     }
-
 }
-
